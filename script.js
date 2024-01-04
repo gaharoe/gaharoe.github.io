2c9ff3feb20f3514a5f00cb8f49f43b2258127ec
gaharoe <chndr3303@gmail.com>
2024-01-04 21:23:24 +0700

    damn timer ready to publish

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
 let slide = document.querySelector("div.slider");
-let check = document.querySelector("nav ul input");
+let alGreen = document.querySelector("div.alGreen");
+let alBlue = document.querySelector("div.alBlue");
+let alRed = document.querySelector("div.alRed");
+let time = document.querySelector("div.box div.timebox h1");
+let chatMe = document.querySelector("div.box div.buttons a");
 
-function changer(){
+let check = document.querySelector("nav ul input");
+let starter = document.querySelector("div.container div.box button");
+let skip = document.querySelector("div.container div.box div.buttons button:nth-child(2)");
+function timeout(){
+  let timer = 10;
+  time.innerHTML = `00:${(timer)}`;
+  timer = timer - 1;
+  let interval = setInterval(count, 1000);
+  function count(){
+    if(timer < 10){time.innerHTML = `00:0${(timer)}`;}
+    else {time.innerHTML = `00:${(timer)}`;}
+    if (timer == 0){
+      alRed.classList.add("Up");
+      alGreen.classList.remove("Up");
+      skip.classList.remove("skipper");
+      chatMe.classList.add("chatMe");
+      let counting = 1;
+      let redDisappear = setInterval(function(){
+        if(counting > 1){
+          alRed.classList.remove("Up");
+          alBlue.classList.remove("Up");
+          clearInterval(redDisappear);
+        }
+        counting++;
+      }, 1000);
+      clearInterval(interval);
+    }
+    timer--;
+  }
+  return;
+}
+
+function slider(){
   slide.classList.toggle("slide");
 }
 
-check.addEventListener('click', changer);
+function alertUp(){
+  alGreen.classList.add("Up");
+  alBlue.classList.add("Up");
+  alRed.classList.add("Up");
+  skip.classList.add("skipper");
+  chatMe.classList.remove("chatMe");
+  timeout();
+}
+
+check.addEventListener('click', slider);
+starter.addEventListener('click', alertUp);
+skip.addEventListener('click', function(){
+  alBlue.classList.add("alBlueUper");
+  let counting = 1;
+  let interval = setInterval(function(){
+    if(counting > 1){
+      alBlue.classList.remove("alBlueUper");
+      clearInterval(interval);
+    }
+  counting++;
+  }, 1000);
+});
